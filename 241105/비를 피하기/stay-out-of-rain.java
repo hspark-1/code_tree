ee593556a85f471b533e601df5b26ebd329bd7be
HyunseoPark <105943940+hspark-1@users.noreply.github.com>
2024-11-05 09:01:43 +0900

    Title: 비를 피하기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241105/비를 피하기/stay-out-of-rain.java
+++ 241105/비를 피하기/stay-out-of-rain.java
@@ -66,5 +66,9 @@ public class Main {
             for (int j=0; j<n; j++) System.out.print(result[i][j] + " ");
             System.out.println();
         }
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
     }
 }
