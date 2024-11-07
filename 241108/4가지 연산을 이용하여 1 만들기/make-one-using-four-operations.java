5e5ca0c7284c40cd77988d3fc60901aee377ec76
HyunseoPark <105943940+hspark-1@users.noreply.github.com>
2024-11-08 08:56:35 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 2271ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241108/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static Queue<int[]> q = new LinkedList<>();
+
+    public static int bfs() {
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            if (curr[0] == 1) return curr[1];
+
+            int n, depth;
+
+            n = curr[0] + 1;
+            depth = curr[1] + 1;
+            q.add(new int[]{n, depth});
+
+            if (curr[0] % 2 == 0) {
+                n = curr[0] / 2;
+                q.add(new int[]{n, depth});
+            }
+
+            if (curr[0] % 3 == 0) {
+                n = curr[0] / 3;
+                q.add(new int[]{n, depth});
+            }
+
+            n = curr[0] - 1;
+            q.add(new int[]{n, depth});
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        q.add(new int[]{N, 0});
+        System.out.println(bfs());
+    }
+}
