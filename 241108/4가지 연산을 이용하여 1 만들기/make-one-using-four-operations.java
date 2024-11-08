33ae92f8e8e6e7c58a37d87bf5f2e465ce40ebf0
HyunseoPark <105943940+hspark-1@users.noreply.github.com>
2024-11-08 12:58:37 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241108/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 241108/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -3,10 +3,13 @@ import java.io.*;
 
 public class Main {
     public static Queue<int[]> q = new LinkedList<>();
+    public static int[] dp;
+    public static int N;
 
     public static int bfs() {
         while(!q.isEmpty()) {
             int[] curr = q.poll();
+            if (N < curr[0]) continue;
             if (curr[0] == 1) return curr[1];
 
             int n, depth;
@@ -14,22 +17,32 @@ public class Main {
 
             if (curr[0] % 3 == 0) {
                 n = curr[0] / 3;
-                q.add(new int[]{n, depth});
+                if (dp[n] == 0) {
+                    q.add(new int[]{n, depth});
+                    dp[n] = depth;
+                }
             }
 
             if (curr[0] % 2 == 0) {
                 n = curr[0] / 2;
-                q.add(new int[]{n, depth});
+                if (dp[n] == 0) {
+                    q.add(new int[]{n, depth});
+                    dp[n] = depth;
+                }
+            }
+            
+            n = curr[0] + 1;
+            if (!(n > N)) {
+                if (dp[n] == 0) {
+                    q.add(new int[]{n, depth});
+                    dp[n] = depth;
+                }
             }
 
-            if (curr[0] + 1 % 2 == 0 || curr[0] + 1 % 3 == 0) {
-                n = curr[0] + 1;
+            n = curr[0] - 1;
+            if (dp[n] == 0) {
                 q.add(new int[]{n, depth});
-            }
-
-            if (curr[0] - 1 % 2 == 0 || curr[0] - 1 % 3 == 0) {
-                n = curr[0] - 1;
-                q.add(new int[]{n, depth});
+                dp[n] = depth;
             }
         }
         return -1;
@@ -39,9 +52,11 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        dp = new int[N+1];
 
         q.add(new int[]{N, 0});
+        dp[N] = 0;
         System.out.println(bfs());
     }
 }
