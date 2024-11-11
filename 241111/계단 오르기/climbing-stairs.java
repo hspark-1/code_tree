ee73b1163b2c90a41198590c1b0ca56138d7cc62
HyunseoPark <105943940+hspark-1@users.noreply.github.com>
2024-11-11 16:53:00 +0900

    Title: 계단 오르기, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241111/계단 오르기/climbing-stairs.java
+++ 241111/계단 오르기/climbing-stairs.java
@@ -2,26 +2,26 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int n;
+    public static int num;
     public static long[] note;
 
     public static long dp(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 1;
+        if (n == 1) return 0;
         if (note[n] == 0) {
-            if (n == 2) return 1;
-            if (n == 3) return 1;
-            if (n == 1) return 0;
             note[n] = dp(n-2) + dp(n-3);
-        }
-        return note[n];
+            return note[n];
+        } else return note[n];
     }
 
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        note = new long[n+1];
+        num = Integer.parseInt(st.nextToken());
+        note = new long[num+1];
 
-        System.out.print(dp(n));
+        System.out.print(dp(num));
     }
 }
