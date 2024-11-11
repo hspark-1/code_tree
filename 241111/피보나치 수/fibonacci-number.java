2810d7e653478b9b58b0a8aac678156bb7ae2ed9
HyunseoPark <105943940+hspark-1@users.noreply.github.com>
2024-11-11 16:32:44 +0900

    Title: 피보나치 수, Time: 85ms, Memory: 8MB, Status: Runtime Error - CodeTree

---
+++ 241111/피보나치 수/fibonacci-number.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int[] note;
+    public static int N;
+
+    public static void fibo(int n) {
+        if (n-1 == N) {
+            System.out.println(note[N]);
+            return;
+        } else {
+            note[n] = note[n-1] + note[n-2];
+            fibo(n+1);
+        }
+        return;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        note = new int[N*N];
+        note[1] = 1;
+        note[2] = 1;
+        fibo(3);
+    }
+}
