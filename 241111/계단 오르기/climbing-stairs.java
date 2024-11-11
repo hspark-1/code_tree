66d39560a1ae6650679a408f10013661fef2367c
HyunseoPark <105943940+hspark-1@users.noreply.github.com>
2024-11-11 16:46:27 +0900

    Title: 계단 오르기, Time: 354ms, Memory: 74MB, Status: Runtime Error - CodeTree

---
+++ 241111/계단 오르기/climbing-stairs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+    public static int[] note;
+
+    public static int dp(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 1;
+        return dp(n-2) + dp(n-3);
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        note = new int[n+1];
+
+        System.out.print(dp(n));
+    }
+}
